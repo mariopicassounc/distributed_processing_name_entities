f823b26f9d02a195f181dbcb61f1b3a79552e4cf
mario <Mario Picasso>
2023-05-28 03:37:58 -0300

    eliminate class ListNamedEntities and merge class with FactoryNamedEntity

--- MarioPicasso/src/namedEntity/FactoryNamedEntity.java
+++ MarioPicasso/src/namedEntity/FactoryNamedEntity.java
@@ -1,5 +1,6 @@
 package namedEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import namedEntity.entities.OtherNE;
@@ -8,17 +9,48 @@ import namedEntity.entities.Organization;
 import namedEntity.entities.PersonEnt.LastName;
 import namedEntity.entities.PlaceEnt.Country;
 
+import namedEntity.heuristic.Heuristic;
 
 import namedEntity.themes.Futbol;
 import namedEntity.themes.International;
 import namedEntity.themes.Technology;
 import namedEntity.themes.Tenis;
 
+import scala.Tuple2;
+
 public class FactoryNamedEntity {
-    public static NamedEntity createNamedEntity(String name, List<String> category, int frequency) {
+
+    private List<NamedEntity> listNamedEntities;
+
+    public List <NamedEntity> createListNamedEntitys(Heuristic h, List<Tuple2<String, Integer>> output) {
+        /*
+         * Create a list of Named Entities from a List of Tuples word: frecuency and a
+         * selected heuristic
+         * Returns List of NE
+         */
+
+        this.listNamedEntities = new ArrayList<NamedEntity>();
+
+        for (Tuple2<?, ?> tuple : output) {
+            String word = (String) tuple._1();
+            Integer frequency = (Integer) tuple._2();
+
+            // If it is a named entity, add it to the list
+            if (h.isEntity(word)) {
+                List<String> category = h.getCategory(word);
+                FactoryNamedEntity factoryNamedEntity = new FactoryNamedEntity();
+                NamedEntity namedEntity = factoryNamedEntity.createNamedEntity(word, category, frequency);
+                this.listNamedEntities.add(namedEntity);
+            }
+        }
+        return listNamedEntities;
+    }
+    
+    private NamedEntity createNamedEntity(String name, List<String> category, int frequency) {
         /*
          * Assumes that the name is a NamedEntity, checked this condition with the
-         * heuristic rules
+         * heuristic rules.
+         * Updates values in the map
          */
 
         // All the cases covered by the heuristic Map
