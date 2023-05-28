648755d6f1b4c68d03255f7bc8d19de0ff140630
mario <Mario Picasso>
2023-05-28 02:17:10 -0300

    Factory class of NE

--- MarioPicasso/src/namedEntity/FactoryNamedEntity.java
+++ MarioPicasso/src/namedEntity/FactoryNamedEntity.java
@@ -3,8 +3,16 @@ package namedEntity;
 import java.util.List;
 
 import namedEntity.entities.OtherNE;
+import namedEntity.entities.Product;
+import namedEntity.entities.Organization;
 import namedEntity.entities.PersonEnt.LastName;
+import namedEntity.entities.PlaceEnt.Country;
+
+
+import namedEntity.themes.Futbol;
 import namedEntity.themes.International;
+import namedEntity.themes.Technology;
+import namedEntity.themes.Tenis;
 
 public class FactoryNamedEntity {
     public static NamedEntity createNamedEntity(String name, List<String> category, int frequency) {
@@ -13,10 +21,33 @@ public class FactoryNamedEntity {
          * heuristic rules
          */
 
+        // All the cases covered by the heuristic Map
+
         if (category.get(0).equals("LastName") && category.get(1).equals("International")) {
             Theme theme = new International();
             return new LastName(name, category, frequency, theme, 0, null, null);
-        } else {
+        } 
+        else if (category.get(0).equals("LastName") && category.get(1).equals("Futbol")){
+            Theme theme = new Futbol();
+            return new LastName(name, category, frequency, theme, 0, null, null);
+        }
+        else if (category.get(0).equals("LastName") && category.get(1).equals("Tenis")){
+            Theme theme = new Tenis();
+            return new LastName(name, category, frequency, theme, 0, null, null);
+        }
+        else if (category.get(0).equals("Country") && category.get(1).equals("International")){
+            Theme theme = new International();
+            return new Country(name, category, frequency, theme, 0, null);
+        }
+        else if (category.get(0).equals("Organization") && category.get(1).equals("Technology")){
+            Theme theme = new Technology();
+            return new Organization(name, category, frequency, theme, null, 0, null);
+        }
+        else if (category.get(0).equals("Product") && category.get(1).equals("Technology")){
+            Theme theme = new Technology();
+            return new Product (name, category, frequency, theme, false, null);
+        }
+        else {
             return new OtherNE(name, category, frequency, null, null);
         }
     }
