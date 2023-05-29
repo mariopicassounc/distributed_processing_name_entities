afd82a070db59ed7f4f5a75eb3da05bb8cf4b5a4
Mateo Malpassi <mateo.malpassi@mi.unc.edu.ar>
2023-05-29 19:51:08 -0300

    Lista artículos

--- Mateo Malpassi/src/FeedReaderMain.java
+++ Mateo Malpassi/src/FeedReaderMain.java
@@ -1,14 +1,18 @@
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
+import feed.Article;
 import feed.Feed;
+
 import parser.FeedParser;
 import parser.FeedParserFactory;
 import parser.SubscriptionParser;
+
 import subscription.SingleSubscription;
 import subscription.Subscription;
 
@@ -41,7 +45,10 @@ public class FeedReaderMain {
 		catch(FileNotFoundException e){
 			System.out.println("Error: " + e.getMessage());
 		}
-				
+		
+		//Lista donde se guardan los artículos de cada feed
+		List<Article> articles = new ArrayList<Article>();
+		
 		// Para cada subscripcion requestear el feed, parsearlo y mostrarlo
 		for (SingleSubscription s: subscription.getSubscriptionsList()){
 			String url = s.getUrl();
@@ -57,9 +64,19 @@ public class FeedReaderMain {
 				FeedParser feedParser = FeedParserFactory.createParser(type);
 				feed = feedParser.parseFeed(feedUrl);
 				feed.prettyPrint();
+
+				//Añado los artículos a la lista
+				articles.addAll(feed.getArticleList());
 			}
 		}
-		
+
+		//Lista de artículos a String
+		List<String> articleToString = new ArrayList<String>();
+		for (Article a: articles){
+			articleToString.add(a.getText());
+			articleToString.add(a.getTitle());
+		}
+
 		// Detener Spark
 		spark.stop();
 	}
