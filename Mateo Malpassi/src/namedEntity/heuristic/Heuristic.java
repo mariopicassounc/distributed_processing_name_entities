1c28c4beb9a95ed8d96db5317e48736735edc1c5
Mateo Malpassi <mateo.malpassi@mi.unc.edu.ar>
2023-05-31 21:47:00 -0300

    Cambio Heuristic ; Nuevo theme

--- Mateo Malpassi/src/namedEntity/heuristic/Heuristic.java
+++ Mateo Malpassi/src/namedEntity/heuristic/Heuristic.java
@@ -9,16 +9,18 @@ public abstract class Heuristic {
 
 	private static Map<String, List<String>> categoryMap = new HashMap<String, List<String>>(){
 		{
-			put("Microsoft", Arrays.asList("Organization", "OtherNE"));
-			put("Apple", Arrays.asList("Organization", "OtherNE"));
-			put("Google", Arrays.asList("Organization", "OtherNE"));
+			put("Microsoft", Arrays.asList("Organization", "Industry"));
+			put("Apple", Arrays.asList("Organization", "Industry"));
+			put("Google", Arrays.asList("Organization", "Industry"));
 			put("Musk", Arrays.asList("LastName", "International"));
-			put("Biden", Arrays.asList("LastName", "International"));
-			put("Trump", Arrays.asList("LastName", "International"));
+			put("Biden", Arrays.asList("LastName", "Politics"));
+			put("Trump", Arrays.asList("LastName", "Politics"));
 			put("Messi", Arrays.asList( "LastName", "Futbol"));
 			put("Federer", Arrays.asList("LastName", "Tenis"));
+			put("Phil Collins", Arrays.asList("LastName", "Music"));
 			put("USA", Arrays.asList("Country", "International"));
 			put("Russia", Arrays.asList("Country", "International"));
+			put("Iphone", Arrays.asList("Product", "Industry"));
 		}
 	};
 	
---
+++ Mateo Malpassi/src/namedEntity/themes/Industry.java
@@ -0,0 +1,5 @@
+package namedEntity.themes;
+
+public class Industry {
+    public Industry(){};
+}
